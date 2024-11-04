7d26b04f6b47a737e0aaaa9dfcdbb49798e62daa
AniKrisn <anirudh.krishnan@hotmail.com>
2024-11-04 17:25:08 +0530

    num, clear, del working

--- calc.js
+++ calc.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 const viewport = document.createElement('viewport');
+const stageView = document.createElement('stageView');
 const clearContainer = document.createElement('clearct');
 const clearButton = document.createElement('clearbtn');
 const delButton = document.createElement('delbtn');
@@ -9,24 +10,35 @@ viewport.className = 'viewport';
 clearContainer.className = 'clearct';
 clearButton.className = 'clearbtn';
 delButton.className = 'delbtn';
+
 container.insertBefore(viewport, container.querySelector('.btn-container'));
 container.insertBefore(clearContainer, container.querySelector('.btn-container'));
 clearContainer.appendChild(clearButton);
 clearContainer.appendChild(delButton);
+clearButton.setAttribute('data-type', 'clear');
+delButton.setAttribute('data-type', 'del');
 
 clearButton.textContent = "CLEAR";
 delButton.textContent = "DELETE";
-viewport.textContent = "0";
+viewport.setAttribute('data-placeholder', '0');
+viewport.textContent = '';
 
 const buttonCount = 16;
 for (let i = 0; i < buttonCount; i++) {
     const button = document.createElement('button'); 
 
-    const buttonTexts = ['7', '8', '9', '÷', '4', '5', '6', 'x', '1', '2', '3', '-', '.', '0', '=', '+'];
-    button.textContent = buttonTexts[i]; 
-    button.className = 'butty';
+    const buttonTexts = 
+    ['7', '8', '9', '÷',
+     '4', '5', '6', '×',
+     '1', '2', '3', '−',
+     '.', '0', '=', '+'];
+    button.textContent = buttonTexts[i];
 
-    const dataAttributes = ['num', 'num', 'num', 'oper', 'num', 'num', 'num', 'oper', 'num', 'num', 'num', 'oper', 'oper', 'num', 'oper', 'oper'];
+    const dataAttributes = 
+    ['num', 'num', 'num', 'oper',
+    'num', 'num', 'num', 'oper',
+    'num', 'num', 'num', 'oper',
+    'oper', 'num', 'oper', 'oper'];
     button.setAttribute('data-type', dataAttributes[i]);
     
     if (i % 4 === 0) {
@@ -37,6 +49,31 @@ for (let i = 0; i < buttonCount; i++) {
     btnContainer.lastChild.appendChild(button);
 }
 
+var res = 0;
+container.addEventListener('click', (e) => {
+    if (e.target.matches('button')) {
+        if (e.target.getAttribute('data-type') === 'num') {
+            viewport.textContent += e.target.textContent;
+            res = viewport.textContent;
+        }
+        else if (e.target.getAttribute('data-type') === 'oper') {
+            if (res) {
+                stage = res;
+                viewport.textContent += e.target.textContent;
+                res = viewport.textContent;
+                console.log(stage);
+                console.log(res);
 
+            }
+        }
+    }
 
+    if (e.target.matches('clearbtn')) {
+        viewport.textContent = '';
+    }
+
+    if (e.target.matches('delbtn')) {
+        viewport.textContent = viewport.textContent.slice(0, -1);
+    }
+});
 
